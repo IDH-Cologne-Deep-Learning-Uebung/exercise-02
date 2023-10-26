59c80d8d7ccaeeb3fc9a44a46bb9fe60196d0d8b
thomas5chneider <115982322+thomas5chneider@users.noreply.github.com>
2023-10-26 13:19:19 +0200

    Finished

--- calc.py
+++ calc.py
@@ -1,6 +1,18 @@
-print("Enter number 1: ")
-num1 = int(input())
-print("Enter number 2: ")
-num2 = int(input())
-print("Sum: " + str(num1+num2))
-print("Product: " + str(num1*num2))
+def main(): 
+  print("Enter 'add' to sum two numbers. Enter 'multi' to multiply two numbers. ")
+  op = str(input())
+  print("Enter number 1: ")
+  num1 = int(input())
+  print("Enter number 2: ")
+  num2 = int(input())
+  if (op == "add"):
+    print("Sum: " + str(num1+num2)) 
+  if (op == "multi"): 
+    print("Product: " + str(num1*num2))
+  return
+
+rr = "y"
+while rr == "y":
+    main()
+    print("Do you want to calculate again? (y/n) ")
+    rr = str(input())
