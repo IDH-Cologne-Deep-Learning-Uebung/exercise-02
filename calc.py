4984b115f94360247e732045bf85ff9551632b5d
Mario Müller <mmuel120@smail.uni-koeln.de>
2023-10-30 20:53:15 +0100

    ex2

--- calc.py
+++ calc.py
@@ -1,6 +1,25 @@
-print("Enter number 1: ")
-num1 = int(input())
-print("Enter number 2: ")
-num2 = int(input())
-print("Sum: " + str(num1+num2))
-print("Product: " + str(num1*num2))
+while True:
+    print("Enter number 1: ")
+    num1 = int(input())
+    print("Enter number 2: ")
+    num2 = int(input())
+    searchOp = True
+    while searchOp:
+        print("Enter operation ('+', 'add', 'addition' or '*', 'multi', 'multiplication'):")
+        op = str(input())
+        if op in ("+", "add", "addition"):
+            searchOp = False
+            print("Sum: " + str(num1 + num2))
+            print("Go again? Enter 'y'")
+            rerun = input()
+            if rerun != 'y':
+                exit()
+        elif op in ("*", "multi", "multiplication"):
+            print("Product: " + str(num1*num2))
+            searchOp = False
+            print("Go again? Enter 'y'")
+            rerun = input()
+            if rerun != 'y':
+                exit()
+        else:
+            print("No Operator found")
