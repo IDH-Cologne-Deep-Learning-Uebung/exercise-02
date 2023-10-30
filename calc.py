03f71a4406768db3fe4472ac054de71553fd4d6c
NilsKEA <nallstae@smail.uni-koeln.de>
2023-10-30 23:30:03 +0100

    first

--- calc.py
+++ calc.py
@@ -1,6 +1,30 @@
-print("Enter number 1: ")
-num1 = int(input())
-print("Enter number 2: ")
-num2 = int(input())
-print("Sum: " + str(num1+num2))
-print("Product: " + str(num1*num2))
+conti = "y"
+while conti=="y":
+
+    print("Enter number 1: ")
+    num1 = int(input())
+    print("Enter number 2: ")
+    num2 = int(input())
+
+    print("Enter operation: ")
+    operation = str(input())
+
+
+    if operation == "add" :
+        print("Sum: " + str(num1+num2))
+
+    if operation == "addition" :
+        print("Sum: " + str(num1+num2))
+
+
+    if operation == "multi" :
+        print("Product: " + str(num1*num2))
+
+    if operation == "multiplication" :
+        print("Product: " + str(num1*num2))
+
+
+    print("Continue? [y/n]: ")
+    conti = str(input())
+    if conti == "y":
+        print()
