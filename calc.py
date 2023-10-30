69655058a041c5c33bf18ece596644f15e503114
WavyLotos shohman4 <shohman4@smail.uni-koeln.de>
2023-10-30 19:20:35 +0100

    Hausaufgabe02

--- calc.py
+++ calc.py
@@ -1,6 +1,15 @@
-print("Enter number 1: ")
-num1 = int(input())
-print("Enter number 2: ")
-num2 = int(input())
-print("Sum: " + str(num1+num2))
-print("Product: " + str(num1*num2))
+while True:
+	print("Enter number 1: ")
+	num1 = int(input())
+	print("Enter number 2: ")
+	num2 = int(input())
+	print("which math operation do you want to use?")
+	if ("addition"): 
+		print("addition": str(num1+num2)
+	else ("multiplikation"):
+		print("multiplication": str(num1*num2)
+			
+	print("wanna try again?")
+	yes = input()
+	if yes = "yes":
+		break
